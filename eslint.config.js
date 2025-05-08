d0bf875152148a880dcdd3897e2dea06f94c554f
hi-rachel <rachel.uiux@gmail.com>
2025-05-08 23:50:22 +0900

    build: next14와 안정적으로 호환되는 eslint로 다운그레이드

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ),
+];
