025f8fe91df13e312994e6bce6b7d861f93da272
hi-rachel <rachel.uiux@gmail.com>
2025-08-12 19:31:57 +0900

    chore: 불필요한 코드 제거

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["images.metmuseum.org", "collectionapi.metmuseum.org"],
     formats: ["image/webp", "image/avif"],
